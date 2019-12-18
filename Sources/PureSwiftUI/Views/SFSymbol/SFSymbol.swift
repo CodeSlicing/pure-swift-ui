58408a76b76787596e234fa271c7b0bffad5f121
Adam Fordyce <adamspostbox-dev@yahoo.com>
2019-12-18 23:48:17 +0000

    Squashed commit of the following:
    commit 00279bf4d3f5eaf56bc2539aee1eb028fb7895b4
    Author: Adam Fordyce <adamspostbox-dev@yahoo.com>
    Date:   Wed Dec 18 23:30:23 2019 +0000
    
        Include all Image specific modifiers in SFSymbol definition
    
    commit e721979a2a392990311b66ee232bc452d084a817
    Author: Adam Fordyce <adamspostbox-dev@yahoo.com>
    Date:   Wed Dec 18 19:40:49 2019 +0000
    
        Add resizable to SFSymbol

--- Sources/PureSwiftUI/Views/SFSymbol/SFSymbol.swift
+++ Sources/PureSwiftUI/Views/SFSymbol/SFSymbol.swift
@@ -3,21 +3,42 @@
 //  
 //
 //  Created by Adam Fordyce on 07/12/2019.
+//  Copyright © 2019 Adam Fordyce. All rights reserved.
 //
 
 public struct SFSymbol: View {
     
-    private let systemName: String
+    private let image: Image
     
     public init(_ sfSymbol: SFSymbolName) {
         self.init(sfSymbol.rawValue)
     }
     
     public init(_ systemName: String) {
-        self.systemName = systemName
+        self.image = Image(systemName: systemName)
     }
     
     public var body: Image {
-        Image(systemName: systemName)
+        image
+    }
+    
+    public func resizable() -> Image {
+        self.image.resizable()
+    }
+    
+    public func resizable(capInsets: EdgeInsets = EdgeInsets(), resizingMode: Image.ResizingMode = .stretch) -> Image {
+        self.image.resizable(capInsets: capInsets, resizingMode: resizingMode)
+    }
+    
+    public func antialiased(_ isAntialiased: Bool) -> Image {
+        self.image.antialiased(isAntialiased)
+    }
+    
+    public func interpolation(_ interpolation: Image.Interpolation) -> Image {
+        self.image.interpolation(interpolation)
+    }
+    
+    public func renderingMode(_ renderingMode: Image.TemplateRenderingMode?) -> Image {
+        self.image.renderingMode(renderingMode)
     }
 }
