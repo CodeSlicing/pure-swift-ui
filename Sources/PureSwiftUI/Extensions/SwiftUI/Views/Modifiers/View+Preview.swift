f77b3a75dde418e19599b20fcff140afc7e4edd5
Adam Fordyce <adam@pureswiftui.com>
2020-11-06 11:45:32 +0000

    Update preview devices to latest models

--- Sources/PureSwiftUI/Extensions/SwiftUI/Views/Modifiers/View+Preview.swift
+++ Sources/PureSwiftUI/Extensions/SwiftUI/Views/Modifiers/View+Preview.swift
@@ -11,38 +11,74 @@ import SwiftUI
 public enum PreviewDeviceName: String, CaseIterable {
 
     case Mac = "Mac"
+
     case iPhone_7 = "iPhone 7"
     case iPhone_7_Plus = "iPhone 7 Plus"
+
     case iPhone_8 = "iPhone 8"
     case iPhone_8_Plus = "iPhone 8 Plus"
+
     case iPhone_SE = "iPhone SE"
+    case iPhone_SE_2nd_gen = "iPhone SE (2nd generation)"
+
     case iPhone_X = "iPhone X"
     case iPhone_XS = "iPhone Xs"
     case iPhone_XS_Max = "iPhone Xs Max"
     case iPhone_XR = "iPhone Xʀ"
+
+    case iPhone_11 = "iPhone 11"
     case iPhone_11_Pro = "iPhone 11 Pro"
     case iPhone_11_Pro_Max = "iPhone 11 Pro Max"
+
+    case iPhone_12 = "iPhone 12"
+    case iPhone_12_Pro = "iPhone 12 Pro"
+    case iPhone_12_Pro_Max = "iPhone 12 Pro Max"
+    case iPhone_12_mini = "iPhone 12 mini"
+    
+    case iPad_mini_2 = "iPad mini 2"
+    case iPad_mini_3 = "iPad mini 3"
     case iPad_mini_4 = "iPad mini 4"
-    case iPad_Air_2 = "iPad Air 2"
-    case iPad_Pro_9_7_inch = "iPad Pro (9.7-inch)"
-    case iPad_Pro_12_9_inch = "iPad Pro (12.9-inch)"
-    case iPad_5th_gen = "iPad (5th generation)"
-    case iPad_Pro_12_9_inch_2nd_gen = "iPad Pro (12.9-inch) (2nd generation)"
-    case iPad_Pro_10_5_inch = "iPad Pro (10.5-inch)"
-    case iPad_6th_gen = "iPad (6th generation)"
-    case iPad_Pro_11_inch = "iPad Pro (11-inch)"
-    case iPad_Pro_12_9_inch_3rd_gen = "iPad Pro (12.9-inch) (3rd generation)"
     case iPad_mini_5th_gen = "iPad mini (5th generation)"
+    
+    case iPad_Air = "iPad Air"
+    case iPad_Air_2 = "iPad Air 2"
     case iPad_Air_3rd_gen = "iPad Air (3rd generation)"
+    case iPad_Air_4th_gen = "iPad Air (4th generation)"
+    
+    case iPad_5th_gen = "iPad (5th generation)"
+    case iPad_6th_gen = "iPad (6th generation)"
+    case iPad_7th_gen = "iPad (7th generation)"
+    
+    case iPad_Pro_9_7_inch = "iPad Pro (9.7-inch)"
+    
+    case iPad_Pro_10_5_inch = "iPad Pro (10.5-inch)"
+    
+    @available(*, obsoleted, renamed: "iPad_Pro_11_inch_1st_gen")
+    case iPad_Pro_11_inch = "do not use"
+    case iPad_Pro_11_inch_1st_gen = "iPad Pro (11-inch) (1st generation)"
+    case iPad_Pro_11_inch_2nd_gen = "iPad Pro (11-inch) (2nd generation)"
+
+    @available(*, obsoleted, renamed: "iPad_Pro_12_9_inch_1st_gen")
+    case iPad_Pro_12_9_inch = "do not use"
+    case iPad_Pro_12_9_inch_1st_gen = "iPad Pro (12.9-inch) (1st generation)"
+    case iPad_Pro_12_9_inch_2nd_gen = "iPad Pro (12.9-inch) (2nd generation)"
+    case iPad_Pro_12_9_inch_3rd_gen = "iPad Pro (12.9-inch) (3rd generation)"
+    case iPad_Pro_12_9_inch_4th_gen = "iPad Pro (12.9-inch) (4th generation)"
+    
     case Apple_TV = "Apple TV"
     case Apple_TV_4K = "Apple TV 4K"
     case Apple_TV_4K_at_1080p = "Apple TV 4K (at 1080p)"
+    
     case Apple_Watch_Series_2_38mm = "Apple Watch Series 2 - 38mm"
     case Apple_Watch_Series_2_42mm = "Apple Watch Series 2 - 42mm"
     case Apple_Watch_Series_3_38mm = "Apple Watch Series 3 - 38mm"
     case Apple_Watch_Series_3_42mm = "Apple Watch Series 3 - 42mm"
     case Apple_Watch_Series_4_40mm = "Apple Watch Series 4 - 40mm"
     case Apple_Watch_Series_4_44mm = "Apple Watch Series 4 - 44mm"
+    case Apple_Watch_Series_5_40mm = "Apple Watch Series 5 - 40mm"
+    case Apple_Watch_Series_5_44mm = "Apple Watch Series 5 - 44mm"
+    case Apple_Watch_Series_6_40mm = "Apple Watch Series 6 - 40mm"
+    case Apple_Watch_Series_6_44mm = "Apple Watch Series 6 - 44mm"
 }
 
 // MARK: ----- PREVIEW DEVICE
