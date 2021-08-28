60dddda703bb81e4ffafc81ae1438cd09c5abb12
Adam Fordyce <adam@pureswiftui.com>
2021-08-28 19:17:46 +0100

    Update to pull in v 1.0.0-beta-2 of PureSwiftUIDesign

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["PureSwiftUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/CodeSlicing/pure-swift-ui-design.git", .exact("1.0.0-beta-1")),
+        .package(url: "https://github.com/CodeSlicing/pure-swift-ui-design.git", .exact("1.0.0-beta-2")),
     ],
     targets: [
         .target(
