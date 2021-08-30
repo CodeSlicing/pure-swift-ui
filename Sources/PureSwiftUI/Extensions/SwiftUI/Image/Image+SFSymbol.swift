891c22122311d268981528d7ad99d715f656dcd2
Brett Best <6104381+Brett-Best@users.noreply.github.com>
2021-08-31 04:53:00 +1000

    Feature: macOS, watchOS, tvOS support (#3)
    * Add callbacks for after and every to account for cases where you want to get timer for after and don't want to use it for every
    
    * Update SFSymbolName to v2 including all appropriate deprecations and availability annotations for backwards compatibility
    
    * Squashed commit of the following:
    
    commit 9bfac36c7d6560252260c82662f83744c86e6f3f
    Author: Adam Fordyce <adam@pureswiftui.com>
    Date:   Sun Nov 1 22:41:01 2020 +0000
    
        Add convenience initialisers to Color
    
    commit 36bf741ef360144c96f4750eb5017ab183ab24a5
    Author: Adam Fordyce <adam@pureswiftui.com>
    Date:   Sun Oct 25 22:17:00 2020 +0000
    
        Remove @inlinable - no longer needed to address segmentation fault introduced in Xcode 11.4 - fixed in 11.5
    
    commit 252427d33015cfc689e29f09164cad92cd351811
    Author: Adam Fordyce <adam@pureswiftui.com>
    Date:   Sun Oct 25 21:31:20 2020 +0000
    
        Add convenience initialisers for Gradients
    
        Add xy scaling for layout guides
        Add convenience initialisers for CGRect
        Add "to" extension to CG types
        Add "sizeScaled" to GeometryProxy
        Add "circle" to Path
    
    * Add xScale/yScale operators to layout guides and configs and include tests
    
    * Remove UINumericType for faster compilation times and remove deprecated code
    
    * Add initialiser on SFSymbol that takes a raw String and remove opacity modifier causing slow builds
    
    * Remove slow performing modifiers and further adopt CGFloat as main type in layout guides
    
    * Clean up redundant modifiers adding default values and migrate preview device names
    
    * Remove argument label from inset call
    
    * Rename relativeArc in Path extension
    
    * Update podspec and readme for v2.0.0
    
    * Add static initialiser for Color and update readme and podspec for v2.0.1
    
    * Add circle overloads to Path for various parameters
    
    * Add extensions for drawing circles on Path
    
    * Added support for macOS, tvOS, watchOS
    
    * Update control points so they work with strokes and fills when part of a closed subpath
    
    * Update readme and podspec for v2.0.3
    
    * Remove old control point logic
    
    * Updated control point rendering for quad curves to match curve styling
    
    * Add convenience properties and functions to GeometryProxy, and UIScreen
    
    Remove conflict with native scaling for Shape
    Add convenience initialisers for Label and SFSymbols
    Add renderingMode to SFSymbol constructor functions
    
    * Resolve ambiguity with native offset modifier on shape
    
    * Fix shadowIfNot when using explicit y offset
    
    Expand signature of static initialisers on color to take color space into account as well as opacity. Add static initialiser for white
    
    * Add static initialisers and convenience functions to CGRect and GeometryProxy
    
    Add tests to cover new functionality in CGRect
    
    * Add support for relative and floating point coordinates
    
    * Add version info for 2.1.0 to readme
    
    Co-authored-by: Adam Fordyce <adam@pureswiftui.com>

--- Sources/PureSwiftUI/Extensions/SwiftUI/Image/Image+SFSymbol.swift
+++ Sources/PureSwiftUI/Extensions/SwiftUI/Image/Image+SFSymbol.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 public extension Image {
-    
+    @available(macOS 11.0, *)
     init(sfSymbol sfsymbol: SFSymbolName) {
         self.init(sfSymbol: sfsymbol.rawValue)
     }
 
+    @available(macOS 11.0, *)
     init(sfSymbol sfsymbolName: String) {
         self.init(systemName: sfsymbolName)
     }
